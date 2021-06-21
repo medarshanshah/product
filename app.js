5ed18ebd52a64f69baa4389be36aabbcddb4461f
medarshanshah <darshanshah.ds2017@gmail.com>
2021-06-22 01:25:10 +0530

    Used Swagger UI for routes

--- app.js
+++ app.js
@@ -1,12 +1,35 @@
 require('dotenv').config()
 const express = require('express')
 const mongoose = require('mongoose')
+const swaggerJsDoc = require('swagger-jsdoc')
+const swaggerUI = require('swagger-ui-express')
 const productRoutes = require('./routes/productRoutes')
 
 
 const app = express()
-
 const PORT = process.env.PORT
+
+//Swagger
+const swaggerOptions = {
+    swaggerDefinition: {
+        openapi: '3.0.0',
+        info: {
+            title: 'Product API',
+            version: '1.0.0',
+            description: 'Product Microservice',
+            contact: {
+                name: "Darshan"
+            },
+            servers: ['jttp://localhost:4000']
+        }
+
+    },
+    apis: ['./routes/*.js']
+}
+
+const swaggerDocs = swaggerJsDoc(swaggerOptions)
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocs))
+
 
 // Atlas Database Connection
 const dbURI = process.env.MONGODB_URI
--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -3,12 +3,89 @@ const productController = require('../controller/productController')
 
 const productRouter = Router()
 
+/**
+ * @swagger
+ * /product:
+ *  get:
+ *      summary: Request to get all the products
+ *      tags: [product]
+ *      responses:
+ *          '200':
+ *              description: A successful response
+ *          '400':
+ *              description: Bad Request. Error in Retrieving products
+ */
 productRouter.get('/product',productController.all_product_list)
+
+/**
+ * @swagger
+ * /product/{id}:
+ *  get:
+ *      summary: Request to get the products by id
+ *      tags: [product]
+ *      parameters:
+ *            - in: path
+ *              name: id
+ *              schema:
+ *                  type: string
+ *              required: true
+ *              description: The product id (Check with 60cf4690536e177c1f44d6ef)
+ *      responses:
+ *          '200':
+ *              description: A successful response. Received the product by given id
+ *          '400':
+ *              description: Bad Request Error in Retrieving product
+ */
 productRouter.get('/product/:id',productController.getProductById)
+
+/**
+ * @swagger
+ * /search:
+ *  get:
+ *      summary: Request to search for the products
+ *      tags: [product]
+ *      responses:
+ *          '200':
+ *              description: The product search was successful
+ *          '400':
+ *              description: Bad Request. Error in Retrieving products
+ */
 productRouter.get('/search', productController.searchProducts)
 
 //categories
+/**
+ * @swagger
+ * /category:
+ *  get:
+ *      summary: Request to get the categories of the products
+ *      tags: [category]
+ *      responses:
+ *          '200':
+ *              description: An array of all the categories of products
+ *          '400':
+ *              description: Bad Request. Error in getting categories
+ */
 productRouter.get('/category', productController.get_categories)
+
+/**
+ * @swagger
+ * /category/{name}:
+ *  get:
+ *      summary: Request to get the products on the basis of category name
+ *      tags: [category]
+ *      parameters:
+ *            - in: path
+ *              name: name
+ *              schema:
+ *                  type: string
+ *              required: true
+ *              description: The category name (Check with books, jewelery, electronics, men's clothing and women's clothing)
+ *      responses:
+ *          '200':
+ *              description: A successful response
+ *          '400':
+ *              description: Bad Request. Error in getting products by category
+ */
 productRouter.get('/category/:name', productController.get_productsByCategory)
 
 module.exports = productRouter
